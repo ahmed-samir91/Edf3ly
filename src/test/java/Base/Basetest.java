7b5833efb26e94b9e18072ea30dbd74232d195a2
ahmed <a.elhadad@tatayab.com>
2020-11-18 21:37:43 +0200

    TCS

--- src/test/java/Base/Basetest.java
+++ src/test/java/Base/Basetest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -28,19 +29,32 @@ public class Basetest {
         catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-
-
     }
     @Test
-    public void AutomatedProduct(){
+    public void AutomatedProduct() throws InterruptedException {
         WebElement ItemURL = driver.findElement(By.name("url"));
         ItemURL.sendKeys("https://www.6pm.com/p/product/9409514");
         ItemURL.submit();
-
-
+        Thread.sleep(1000);
+        WebElement EnterSize = driver.findElement(By.name("size"));
+        EnterSize.sendKeys("10");
+        EnterSize.submit();
+        Thread.sleep(1000);
+        WebElement AddItem = driver.findElement(By.xpath("//input[@value='Add item']"));
+        AddItem.click();
+        Thread.sleep(5000);
+        WebElement CartDetails = driver.findElement(By.xpath("//div[contains(text(),'Cart Details')]"));
+        CartDetails.isDisplayed();
     }
-
+    @Test
+    public void ProhibitedProduct() throws InterruptedException {
+        WebElement ItemURL = driver.findElement(By.name("url"));
+        ItemURL.sendKeys("https://www.abercrombie.com/shop/wd/p/skinny-suede-belt-41330319?categoryId=12266&seq=02&faceout=prod1\n");
+        ItemURL.submit();
+        Thread.sleep(5000);
+        WebElement ProhibitedMessage = driver.findElement(By.xpath("//label[contains(text(),'we apologize, store is not available at this momen')]"));
+        ProhibitedMessage.isDisplayed();
+    }
     @AfterClass
     public void tearDown() {
         if (driver != null) {
